5dd505e066165718c589daa5886bcae6a8d4069f
Starza4TW <starza4tw@gmail.com>
2015-08-17 19:03:38 -0400

    Added Command Filtering
    Renamed Permission filter to chatfilter.
    New Permission commandfilter.
    Commands are now filtered in the same way that chat is.

--- src/main/java/com/starza4tw/picturebook/ChatHandler.java
+++ src/main/java/com/starza4tw/picturebook/ChatHandler.java
@@ -3,13 +3,29 @@ package com.starza4tw.picturebook;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class ChatHandler implements Listener
 {
 	@EventHandler
 	public void onPlayerChat(AsyncPlayerChatEvent event)
 	{
-		if(event.getPlayer().hasPermission("picturebook.filter") == true)
+		if(event.getPlayer().hasPermission("picturebook.chatfilter") == true)
+		{
+			for(String Key : ConfigHandler.FilterList.keySet())
+			{
+				if(event.getMessage().toLowerCase().contains(Key.toLowerCase()))
+				{
+					event.setMessage(event.getMessage().replaceAll("(?i)" + Key, ConfigHandler.FilterList.get(Key)));
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerCommand(PlayerCommandPreprocessEvent event)
+	{
+		if(event.getPlayer().hasPermission("picturebook.commandfilter") == true)
 		{
 			for(String Key : ConfigHandler.FilterList.keySet())
 			{
